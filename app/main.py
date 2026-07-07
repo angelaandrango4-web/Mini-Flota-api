a7ac453760718a717f3c537038613fd4f2eceac4
Angela Andrango <angelaandrango4@gmail.com>
2026-07-07 02:39:43 -0500

    feat: connect to mongodb and verify in health endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from app.config import settings
 
 app = FastAPI()
 
+client = AsyncIOMotorClient(settings.mongodb_url)
+database = client["mini_flota"]
+
+
 @app.get("/health")
-async def healt_check():
-    return{"status": "ok"}
-
-
+async def health_check():
+    await database.command("ping")
+    return {"status": "ok"}
